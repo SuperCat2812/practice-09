42524319ce5b8203361a9115b7dcf3605e5bd86a
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-04-23 09:47:21 +0000

    Deploying to gh-pages from @ SuperCat2812/practice-09@8a2ded6773769ff36a7cdcafdf963e43b260ded8 🚀

---
+++ notifications-task.js
@@ -0,0 +1,2 @@
+import{a as t,r as e,b as i}from"./assets/handlers-vUwTWrOY.js";/* empty css                      */document.addEventListener("DOMContentLoaded",t);e.notificationsToggle.addEventListener("change",i);
+//# sourceMappingURL=notifications-task.js.map
